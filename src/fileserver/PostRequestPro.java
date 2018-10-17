a46a3a3e4355cf99d38f8c1ab96fa3fc4ca91cfa
JiLoveZn <2031265447@qq.com>
2018-10-17 18:30:14 +0800

    dataBaseConnection

--- src/fileserver/PostRequestPro.java
+++ src/fileserver/PostRequestPro.java
@@ -1,6 +1,9 @@
 package fileserver;
 
+import java.io.FileOutputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**  
  * @author 陈吉
@@ -9,7 +12,36 @@ import java.sql.Connection;
  */
 
 public class PostRequestPro {
-	private static Connection connection;
+	private static Connection conn;
 	private String filePath;
 	private String fileType;
+	
+	FileOutputStream fileOutputStream;
+	
+	public static void dataBaseConnection(){
+		String db_driver = "com.mysql.jdbc.Driver";
+		String db_username = "root";
+		String db_password = "123456";
+		String db_url = "jdbc:mysql://127.0.0.1:3306/file";
+		try{
+			Class.forName(db_driver);
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+			System.out.println("数据库驱动加载失败");
+		}
+		try{
+			conn = DriverManager.getConnection(db_url, db_username, db_password);
+		}catch(SQLException e){
+			e.printStackTrace();
+			System.out.println("数据库连接失败");
+		}finally{
+			try{
+				conn.close();
+			}catch(Exception e){
+				e.printStackTrace();
+				System.out.println("数据库连接关闭异常");
+			}
+			
+		}
+	}
 }
